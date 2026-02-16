5d2bebfd24bc88a6906e99462fa9132012225cd6
Khrisendat Persaud <khrisendatpersaud@Khrisendats-Mac-mini.local>
2026-02-16 10:23:50 -0500

    fix: Print cleaned text in Speaker class

--- src/hardware/speaker.py
+++ src/hardware/speaker.py
@@ -18,7 +18,7 @@ class Speaker:
     def say(self, text):
         synthesis_input = texttospeech.SynthesisInput(text=text)
         try:
-            print(f"Speaking: {text}")
+            print(f"Speaking: {cleaned_text}")
             response = self.client.synthesize_speech(
                 input=synthesis_input, voice=self.voice, audio_config=self.audio_config
             )
